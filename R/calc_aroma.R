ff7943c6b16151fbbd58401acbb1296e343f020e
PNL\thom040 <allison.thompson@pnnl.gov>
2018-03-01 15:38:27 -0800

    Updated equations to reflect results from published errata

--- R/calc_aroma.R
+++ R/calc_aroma.R
@@ -4,11 +4,11 @@
 #' 
 #' @param icrData an object of class 'peakIcrData' or 'compoundIcrData', typically a result of \code{\link{as.peakIcrData}} or \code{\link{mapPeaksToCompounds}}.
 #' 
-#' @details AI \eqn{= \frac{1 + C - O - S - 0.5H}{C - O - S - N - P}}{= (1 + C - O - S - 0.5* H)/(C - O - S - N - P)} and AI_MOD \eqn{= \frac{1 + C - 0.5O - S - 0.5H}{C - 0.5*O - S - N - P}}{= (1 + C - 0.5*O - S - 0.5*H)/(C - 0.5*O - S - N - P)}. When the numerator or denomenator is <=0, AI and AI_MOD = 0.
+#' @details AI \eqn{= \frac{1 + C - O - S - 0.5(N + P + H)}{C - O - S - N - P}}{= (1 + C - O - S - 0.5*(N + P + H))/(C - O - S - N - P)} and AI_MOD \eqn{= \frac{1 + C - 0.5O - S - 0.5(N + P + H)}{C - 0.5*O - S - N - P}}{= (1 + C - 0.5*O - S - 0.5*(N + P + H))/(C - 0.5*O - S - N - P)}. When the numerator or denomenator is <=0, AI and AI_MOD = 0.
 #' When AI > 0.5 a compound is considered not aromatic, 
 #' 0.5 < = AI <0.67 a compound is aromatic, and when AI >= 0.67 a compound is a condensed aromatic
 #' 
-#' @references Koch, B. P., & Dittmar, T. (2006). From mass to structure: an aromaticity index for high‐resolution mass data of natural organic matter. Rapid communications in mass spectrometry, 20(5), 926-932.
+#' @references Koch, B. P., & Dittmar, T. (2006). From mass to structure: an aromaticity index for high‐resolution mass data of natural organic matter. Rapid communications in mass spectrometry, 20(5), 926-932. Errata: Koch, B. P., & Dittmar, T. (2016). From mass to structure: an aromaticity index for high-resolution mass data of natural organic matter. Rapid communications in mass spectrometery, 30(1), 250. DOI: 10.1002/rcm.7433
 #' 
 #' @return an object of the same class as \code{icrData} with a column in \code{e\_meta} giving aromaticity and modified aromaticity values
 #'  
@@ -24,14 +24,27 @@ calc_aroma <- function(icrData){
   # pull e_meta out of icrData #
   temp = icrData$e_meta
   
-  ai_num = (1 + temp[,getCarbonColName(icrData)] - temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - 0.5*temp[,getHydrogenColName(icrData)])
-  ai_denom = (temp[,getCarbonColName(icrData)] - temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - temp[,getNitrogenColName(icrData)] - temp[,getPhosphorusColName(icrData)])
+  # calculate AI numerator and denominator #
+  
+  # ai_num = (1 + temp[,getCarbonColName(icrData)] - temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - 0.5*temp[,getHydrogenColName(icrData)])
+  # ai_denom = (temp[,getCarbonColName(icrData)] - temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - temp[,getNitrogenColName(icrData)] - temp[,getPhosphorusColName(icrData)])
+  
+  ai_num <- (1 + temp[,getCarbonColName(icrData)] - temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - 
+               0.5*(temp[,getNitrogenColName(icrData)] + temp[,getPhosphorusColName(icrData)] + temp[,getHydrogenColName(icrData)]))
+  ai_denom <- (temp[,getCarbonColName(icrData)] - temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - temp[,getNitrogenColName(icrData)] - temp[,getPhosphorusColName(icrData)]) 
+  
   
   # pull out automatic AI <= 0 cases #
   ai_ids = which(ai_num <= 0 | ai_denom <= 0)
 
-  aimod_num = (1 + temp[,getCarbonColName(icrData)] - 0.5*temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - 0.5*temp[,getHydrogenColName(icrData)])
-  aimod_denom = (temp[,getCarbonColName(icrData)] - 0.5*temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - temp[,getNitrogenColName(icrData)] - temp[,getPhosphorusColName(icrData)])
+  # calculate AImod numberator and denominator #
+  
+  # aimod_num = (1 + temp[,getCarbonColName(icrData)] - 0.5*temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - 0.5*temp[,getHydrogenColName(icrData)])
+  # aimod_denom = (temp[,getCarbonColName(icrData)] - 0.5*temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - temp[,getNitrogenColName(icrData)] - temp[,getPhosphorusColName(icrData)])
+  
+  aimod_num <- (1 + temp[,getCarbonColName(icrData)] - 0.5*temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - 
+                  0.5*(temp[,getNitrogenColName(icrData)] + temp[,getPhosphorusColName(icrData)] + temp[,getHydrogenColName(icrData)]))
+  aimod_denom <- (temp[,getCarbonColName(icrData)] - 0.5*temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - temp[,getNitrogenColName(icrData)] - temp[,getPhosphorusColName(icrData)])
   
   # pull out automatic AI = 0 cases #
   aimod_ids = which(aimod_num <= 0 | aimod_denom <= 0)
--- R/calc_dbe.R
+++ R/calc_dbe.R
@@ -4,13 +4,13 @@
 #' 
 #' @param icrData an object of class 'peakIcrData' or 'compoundIcrData', typically a result of \code{\link{as.peakIcrData}} or \code{\link{mapPeaksToCompounds}}.
 #' 
-#' @details DBE \eqn{= 1 + 0.5(2C - H + N + P)}{= 1 + 0.5*(2C - H + N + P)} and DBE-0 \eqn{= 1 + 0.5(2C - H + N + P) - O}{= 1 + 0.5*(2C - H + N + P) - O}
+#' @details DBE \eqn{= 1 + C - O - S - 0.5(N + P + H)}{= 1 + C - O - S - 0.5*(N + P + H)} and DBE-0 \eqn{= 1 + C - O - S - 0.5(N + P + H) - O}{= 1 + C - O - S - 0.5*(N + P + H) - O}
 #' 
-#' @references Koch, B. P., & Dittmar, T. (2006). From mass to structure: an aromaticity index for high‐resolution mass data of natural organic matter. Rapid communications in mass spectrometry, 20(5), 926-932.
+#' @references Koch, B. P., & Dittmar, T. (2006). From mass to structure: an aromaticity index for high‐resolution mass data of natural organic matter. Rapid communications in mass spectrometry, 20(5), 926-932. Errata: Koch, B. P., & Dittmar, T. (2016). From mass to structure: an aromaticity index for high-resolution mass data of natural organic matter. Rapid communications in mass spectrometery, 30(1), 250. DOI: 10.1002/rcm.7433
 #' 
 #' @return an object of the same class as \code{icrData} with a column in \code{e\_meta} giving DBE and DBE-O values
 #'  
-#' @author Lisa Bramer
+#' @author Lisa Bramer, Allison Thompson
 #' 
 
 calc_dbe <- function(icrData){
@@ -22,8 +22,12 @@ calc_dbe <- function(icrData){
   # pull e_meta out of icrData #
   temp = icrData$e_meta
   
-  temp$DBE = 1 + 0.5*(2*temp[,getCarbonColName(icrData)] - temp[,getHydrogenColName(icrData)] + temp[,getNitrogenColName(icrData)] + temp[,getPhosphorusColName(icrData)])
+  temp$DBE = 1 + temp[,getCarbonColName(icrData)] - temp[,getOxygenColName(icrData)] - temp[,getSulfurColName(icrData)] - 
+    0.5*(temp[,getNitrogenColName(icrData)] + temp[,getPhosphorusColName(icrData)] + temp[,getHydrogenColName(icrData)])
   temp$DBE_O = temp$DBE - temp[,getOxygenColName(icrData)]
+  
+  # Old formula, from original paper
+  #temp$DBE = 1 + 0.5*(2*temp[,getCarbonColName(icrData)] - temp[,getHydrogenColName(icrData)] + temp[,getNitrogenColName(icrData)] + temp[,getPhosphorusColName(icrData)])
   
   if(length(which(is.na(temp[,getMFColName(icrData)]))) > 0){
     temp$DBE[which(is.na(temp[,getMFColName(icrData)]))] = NA
