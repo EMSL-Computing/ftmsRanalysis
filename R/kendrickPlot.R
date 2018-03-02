7c75065dfb2758b05f191b2c96d2342aee0ac222
Amanda White <Amanda.White@pnnl.gov>
2018-03-01 17:19:39 -0800

    Kendrick params now match Van Krevelen params

--- R/kendrickPlot.R
+++ R/kendrickPlot.R
@@ -3,66 +3,129 @@
 #' Constructs a Kendrick plot (Kendrick defect vs Kendrick mass) for an
 #' object of type icrData.
 #' 
-#' @param data.obj icrData object
-#' @param title (optional) plot title
-#' @param colors (optional) named vector of colors or hex codes corresponding to Van Krevelen categories
+#' @param dataObj icrData object
+#' @param title plot title
+#' @param colorPal color palette function, one of \code{\link{col_numeric}}, \code{\link{col_factor}} or similar \code{scales} palette function
+#' @param colorCName column name of \code{dataObj$e_meta} to use for coloring the points. If NA and vkBoundarySet is provided, the points will be colored according to the Van Krevelen category.
+#' @param vkBoundarySet character vector specifying which boundary set to use when determining class. Valid options are currently "bs1" and "bs2" and defaults to "bs1". See details of \code{\link{assign_class}} for differences in sets.
 #' @param xlabel x axis label, default is "Kendrick Mass"
 #' @param ylabel y axis label, default is "Kendrick Defect"
+#' @param legendTitle title for the legend, only used when coloring points according to a numeric scale
 #' @return a plotly object
 #' @seealso \code{\link{plot_ly}}
 #' @export
-kendrickPlot <- function(data.obj, title=NA, colors=NA, xlabel="Kendrick Mass", ylabel="Kendrick Defect", boundary_set = "bs1") {
+kendrickPlot <- function(dataObj, title=NA, colorPal=NA, colorCName=NA, vkBoundarySet = "bs1", 
+                         xlabel="Kendrick Mass", ylabel="Kendrick Defect", legendTitle=colorCName) {
   require(dplyr)
   require(plotly)
   require(scales)
   
   # Test inputs
-  if (!inherits(data.obj, "icrData")) {
-    stop("data.obj must be of type icrData")
+  if (!inherits(dataObj, "icrData")) {
+    stop("dataObj must be of type icrData")
   }
-  if (is.null(data.obj$e_meta)) {
-    stop("data.obj must have e_meta element")
-  }
-  
-  km.col <- getKendrickMassColName(data.obj)
-  if (is.null(km.col) | !is.element(km.col, colnames(data.obj$e_meta))) {
-    stop("Kendrick mass column attribute is not set or is not present in data.obj$e_meta")
-  }
-  kd.col <- getKendrickDefectColName(data.obj)
-  if (is.null(kd.col) | !is.element(kd.col, colnames(data.obj$e_meta))) {
-    stop("Kendrick defect column attribute is not set or is not present in data.obj$e_meta")
+  if (is.null(dataObj$e_meta)) {
+    stop("dataObj must have e_meta element")
   }
   
-  # Van Krevelen categories for coloring points
-  emeta.df <- data.obj$e_meta
-  emeta.df[,getEDataColName(data.obj)] = as.character(emeta.df[,getEDataColName(data.obj)])
-
-  emeta.df$Class <- getVanKrevelenCategories(data.obj, boundary_set = boundary_set)
+  km_col <- getKendrickMassColName(dataObj)
+  if (is.null(km_col) | !is.element(km_col, colnames(dataObj$e_meta))) {
+    stop("Kendrick mass column attribute is not set or is not present in dataObj$e_meta")
+  }
+  kd_col <- getKendrickDefectColName(dataObj)
+  if (is.null(kd_col) | !is.element(kd_col, colnames(dataObj$e_meta))) {
+    stop("Kendrick defect column attribute is not set or is not present in dataObj$e_meta")
+  }
+  
+  if (is.na(colorCName) & is.na(vkBoundarySet)) {
+    stop("at least one of colorCName or vkBoundarySet must be specified")
+  }
+  
+  emeta.df <- dataObj$e_meta
+  emeta.df[,getEDataColName(dataObj)] = as.character(emeta.df[,getEDataColName(dataObj)])
+  
+  # Van Krevelen categories
+  if (!is.na(vkBoundarySet) & is.na(colorCName)) {
+    vk_cat <- factor(getVanKrevelenCategories(dataObj, vkBoundarySet), levels=rownames(getVanKrevelenCategoryBounds(vkBoundarySet)))
+    emeta.df$vk_categories_really_long_name1234 <- vk_cat
+    colorCName <- "vk_categories_really_long_name1234"
+  }
+  
+  # color palette
+  if (is.na(colorPal)) {
+    if (is.numeric(emeta.df[, colorCName])) {
+      val_range <- range(emeta.df[, colorCName], na.rm=TRUE)
+      colorPal <- scales::col_numeric(palette="YlOrRd", domain=val_range)
+      vals <- seq(val_range[1], val_range[2], length=100)
+      col_vec <- colorPal(vals)
+      names(col_vec) <- vals
+    } else if (is.factor(emeta.df[, colorCName])) {
+      cc <- levels(emeta.df[, colorCName])
+      colorPal<- getFactorColorPalette(cc)  
+      col_vec <- colorPal(cc)
+      names(col_vec) <- cc
+    } else if (is.character(emeta.df[, colorCName])) {
+      cc <- sort(unique(emeta.df[, colorCName]))
+      emeta.df[, colorCName] <- factor(emeta.df[, colorCName], levels = cc)
+      colorPal<- getFactorColorPalette(cc)  
+      col_vec <- colorPal(cc)
+      names(col_vec) <- cc
+    } else {
+      stop(sprintf("invalid data type for column '%s': %s (must be numeric, factor or character)", colorCName, data.class(emeta.df[, colorCName])))
+    }
+    
+  } else { # check that supplied color palette is applicable to the colorCName
+    if (is.numeric(emeta.df[, colorCName])) {
+      if (!(attr(colorPal, "colorType") %in% c("numeric", "bin", "quantile"))) {
+        stop("the provided color palette is not applicable to the chosen color column")
+      }
+      pal_domain <- sort(get("domain", environment(colorPal)))
+      vals <- seq(pal_domain[1], pal_domain[2], length=100)
+    } else if (class(emeta.df[, colorCName]) %in% c("factor", "character")) {
+      if (attr(colorPal, "colorType") != "factor") {
+        stop("the provided color palette is not applicable to the chosen color column")
+      } 
+      vals <- get("domain", environment(colorPal))
+      if (all(is.null(vals))) vals <- get("levels", environment(colorPal))
+      if (all(is.null(vals))) vals <- unique(emeta.df[, colorCName])
+    }
+    col_vec <- colorPal(vals)
+    names(col_vec) <- vals
+  }
   
   # Include only rows (peaks) where that are observed in at least one column of e_data
-  obs.peaks <- as.character(data.obj$e_data[data.obj$e_data[,-which(names(data.obj$e_data) == getEDataColName(data.obj))] > 0, getEDataColName(data.obj)])
-  emeta.df <- emeta.df[which(emeta.df[,getEDataColName(data.obj)] %in% obs.peaks), ]
-  
-  # Color palette
-  if (is.na(colors)) {
-    col.pal<- scales::col_factor("Set1", levels=rownames(getVanKrevelenCategoryBounds(boundary_set)))   
-    col.vec <- col.pal(rownames(getVanKrevelenCategoryBounds(boundary_set)))
-    names(col.vec) <- rownames(getVanKrevelenCategoryBounds(boundary_set))
-  } else if (length(colors) == 9) {
-    col.vec <- colors
+  samp_cnames <- as.character(dataObj$f_data[, getFDataColName(dataObj)])
+  if(length(samp_cnames) == 1){
+    ind <- dataObj$e_data[,samp_cnames] >0
+  }else{
+    ind <- rowSums(dataObj$e_data[, samp_cnames]) > 0
   }
   
-  # get some column names #
-  oc_id = getOCRatioColName(data.obj)
-  hc_id = getHCRatioColName(data.obj)
-  mf_id = getMFColName(data.obj)
-  # Mouseover text (TODO add function parameter for this)
-  hovertext <- sprintf("%s<br>%s<br>O:C=%1.3f<br>H:C=%1.3f", emeta.df[,mf_id], emeta.df$Class, emeta.df[,oc_id], emeta.df[,hc_id])
+  obs.peaks <- as.character(dataObj$e_data[ind, getEDataColName(dataObj)])
+  emeta.df <- emeta.df[which(emeta.df[,getEDataColName(dataObj)] %in% obs.peaks), ]
   
-  p <- plot_ly(emeta.df, x=emeta.df[,km.col], y=emeta.df[,kd.col]) %>%
-    add_markers(color=~Class, colors=col.vec, text=hovertext, hoverinfo="text") %>%
-    layout(xaxis=list(title="Kendrick Mass", range=nice_axis_limits(emeta.df[, km.col])), 
-           yaxis=list(title="Kendrick Defect", range=nice_axis_limits(emeta.df[, kd.col])))
+  ind.na <- is.na(emeta.df[,km_col]) | is.na(emeta.df[,kd_col])
+  emeta.df <- emeta.df[!ind.na, ]
+  
+  ed_id = getEDataColName(dataObj)
+  # Mouseover text (TODO add function parameter for this)
+  hovertext <- sprintf("%s<br>Kendrick Mass=%1.3f<br>Kendrick Defect=%1.3f", emeta.df[,ed_id], emeta.df[,km_col], emeta.df[,kd_col])
+  
+  if (!is.numeric(emeta.df[, colorCName])) {
+    p <- plot_ly(emeta.df, x=emeta.df[,km_col], y=emeta.df[,kd_col]) %>%
+#      add_trace(name=legendTitle, mode="none", type="scatter", opacity=0) %>% # empty trace in order to add legend title
+      add_markers(color=emeta.df[, colorCName], colors=col_vec, text=hovertext, hoverinfo="text") %>%
+      layout(xaxis=list(title=xlabel, range=nice_axis_limits(emeta.df[, km_col])), 
+             yaxis=list(title=ylabel, range=nice_axis_limits(emeta.df[, kd_col])))
+  } else { #NUMERIC COLOR COLUMN:
+    p <- plot_ly(emeta.df, x=emeta.df[,km_col], y=emeta.df[,kd_col]) %>%
+      add_markers(color=emeta.df[, colorCName], colors=col_vec, text=hovertext, hoverinfo="text",
+                  marker = list(colorbar = list(title = legendTitle))) %>%
+      layout(xaxis=list(title=xlabel, range=nice_axis_limits(emeta.df[, km_col])), 
+             yaxis=list(title=ylabel, range=nice_axis_limits(emeta.df[, kd_col])))
+  }
+  
+  
   if (!is.na(title)) {
     p <- p %>% layout(title=title)
   }
@@ -70,4 +133,4 @@ kendrickPlot <- function(data.obj, title=NA, colors=NA, xlabel="Kendrick Mass", 
   
 }
 
-#kendrickPlot(data.obj, title=colnames(data.obj$e_data)[2])
+#kendrickPlot(dataObj, title=colnames(dataObj$e_data)[2])
