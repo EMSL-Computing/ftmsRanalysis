ae757a43b07263ccfd32dc14410f478a467f6112
Amanda White <Amanda.White@pnnl.gov>
2018-05-14 17:23:01 -0700

    moved vignette and added help file to show how to launch it

---
+++ R/vignette_interactivity_between_plots.R
@@ -0,0 +1,13 @@
+#' Vignette: Interactivity Between Plotly Plots
+#' 
+#' This vignette shows how to link two Plotly plots produced by fticRanalysis functions in Shiny such that 
+#' selections in one plot can be highlighted in the other. 
+#' 
+#' This vignette is an RMarkdown file using Shiny, and may be launched with the command:
+#' 
+#' \code{rmarkdown::run(system.file("doc/interactivity_between_plots.Rmd", package="fticRanalysis"))}
+#' 
+#' @author Amanda White, Sarah Reehl
+#' @rdname vignette_interactivity_between_plots
+#' @name vignette_interactivity_between_plots
+NULL
