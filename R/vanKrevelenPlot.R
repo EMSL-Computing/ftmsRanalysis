bfbb0ede6770ecba58dae845e6705eda33d59420
Amanda White <Amanda.White@pnnl.gov>
2018-06-21 15:13:31 -0700

    adapt VK to use scatterPlot function

--- R/vanKrevelenPlot.R
+++ R/vanKrevelenPlot.R
@@ -46,6 +46,71 @@ vanKrevelenPlot <- function(icrData, title=NA, colorPal=NA, colorCName=NA, vkBou
 
 # internal only function to do the work for both sample and group level VK plots
 .vanKrevelenPlotInternal <- function(icrData, title=NA, colorPal=NA, colorCName=NA, vkBoundarySet = "bs1", showVKBounds=TRUE, 
+                                             xlabel="O:C Ratio", ylabel="H:C Ratio", legendTitle=colorCName, logColorCol=FALSE) {
+  
+  # Test inputs
+  # check that icrData is of the correct class #
+  if(!inherits(icrData, "peakIcrData") & !inherits(icrData, "compoundIcrData")) stop("icrData must be an object of class 'peakIcrData' or 'compoundIcrData'")
+  
+  OC.col <- getOCRatioColName(icrData)
+  if (is.null(OC.col) | !is.element(OC.col, colnames(icrData$e_meta))) {
+    stop("O:C ratio column attribute is not set or is not present in icrData$e_meta")
+  }
+  HC.col <- getHCRatioColName(icrData)
+  if (is.null(HC.col) | !is.element(HC.col, colnames(icrData$e_meta))) {
+    stop("H:C ratio column attribute is not set or is not present in icrData$e_meta")
+  }
+  
+  if (is.na(colorCName) & is.na(vkBoundarySet)) {
+    stop("at least one of colorCName or vkBoundarySet must be specified")
+  }
+  
+  vk_color_different_than_pts <- FALSE
+  if (!is.na(vkBoundarySet)) {
+    icrData <- assign_class(icrData, boundary_set = vkBoundarySet)
+    icrData$e_meta$Class <- gsub(";.*", "", icrData$e_meta$Class)
+    
+    # data.frame of van krevelen bounds for plotting purposes
+    vankrev_categories <- getVanKrevelenPlottingDF(vkBoundarySet)
+    
+    if (showVKBounds & !is.na(colorCName)) {
+      vk_color_different_than_pts <- TRUE
+    }  
+    
+    if (is.na(colorCName)) colorCName <- "Class"
+    
+  }
+  
+  xrange <- nice_axis_limits(icrData$e_meta[, OC.col], zero.min=TRUE)
+  yrange <- nice_axis_limits(icrData$e_meta[, HC.col], zero.min=TRUE)
+
+  # Show Mass and Molecular Formula
+  hovertext <- paste("Molecular Formula: ", icrData$e_meta[, getMFColName(icrData)],"<br>", getEDataColName(icrData),": ", icrData$e_meta[,getEDataColName(icrData)], sep = "")
+  icrData$e_meta$Hover <- hovertext
+  
+  p <- scatterPlot(icrData, OC.col, HC.col, colorCName = colorCName, colorPal=colorPal, xlabel=xlabel, ylabel=ylabel,
+                   legendTitle=legendTitle, title=title, xrange=xrange, yrange=yrange, logColorCol=logColorCol, hoverTextCName="Hover")
+  style(p, hoverinfo="text", text=hovertext, traces=1)
+  
+  if (showVKBounds) {
+    if (vk_color_different_than_pts) {
+      # if vk boxes are drawn but points are not colored according to vk category, color boxes black
+      p <- p %>%
+        add_segments(x=~x0, y=~y0, xend=~x1, yend=~y1, data=vankrev_categories, 
+                     text=vankrev_categories$category, hoverinfo="text", line=list(color="#000000"), showlegend = FALSE) 
+    } else {
+      p <- p %>%
+        add_segments(x=~x0, y=~y0, xend=~x1, yend=~y1, data=vankrev_categories, color=~category,
+                     text=vankrev_categories$category, hoverinfo="text", showlegend = FALSE) 
+    }
+  }
+
+  p
+}
+
+
+# internal only function to do the work for both sample and group level VK plots
+.vanKrevelenPlotInternalOriginal <- function(icrData, title=NA, colorPal=NA, colorCName=NA, vkBoundarySet = "bs1", showVKBounds=TRUE, 
                                      xlabel="O:C Ratio", ylabel="H:C Ratio", legendTitle=colorCName, logColorCol=FALSE) {
   
   # Test inputs
