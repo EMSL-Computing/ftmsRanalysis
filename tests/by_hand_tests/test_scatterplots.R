f5f0a0bffb7e4948fcd64c5d5f1900c5fd5d4982
Amanda White <Amanda.White@pnnl.gov>
2018-06-21 15:13:10 -0700

    generic scatter plot function

---
+++ tests/by_hand_tests/test_scatterplots.R
@@ -0,0 +1,47 @@
+## Test scatter plot function
+
+library(fticRanalysis)
+
+data(peakIcrProcessed)
+
+icrData <- peakIcrProcessed
+xCName <- "NOSC"
+yCName <- "DBE"
+colorCName <- "HtoC_ratio"
+
+# basic scatterplot
+scatterPlot(icrData, xCName, yCName, colorCName)
+
+# try setting the x and y ranges and hover info
+icrData$e_meta$Hover <- sprintf("H:C Ratio: %.4f", icrData$e_meta$HtoC_ratio)
+scatterPlot(icrData, xCName, yCName, colorCName, xrange=c(-2, 2), yrange=c(-35, 35), hoverTextCName="Hover")
+
+# custo mcolor palette, legend title, and plot title
+colorPal <- scales::col_numeric("YlGnBu", domain = range(icrData$e_meta$HtoC_ratio, na.rm=TRUE))
+scatterPlot(icrData, xCName, yCName, colorCName, colorPal = colorPal, legendTitle="H:C Ratio", title="Test")
+
+# color by an integer valued column
+scatterPlot(icrData, xCName, yCName, colorCName="N")
+
+# color by a factor
+icrData <- assign_class(icrData)
+icrData$e_meta$Class <- gsub(";.*", "", icrData$e_meta$Class)
+scatterPlot(icrData, xCName, yCName, colorCName="Class", xlabel = "My X Label", ylabel="My Y Label")
+
+# don't specify a color column
+scatterPlot(icrData, xCName, yCName)
+
+# custom constant color for points
+scatterPlot(icrData, xCName, yCName, colorPal="#FF0000")
+
+# log transforming vs not: look at the difference in color bar range
+scatterPlot(icrData, xCName, yCName, colorCName="EM0013_sample", logColorCol = TRUE, legendTitle="EM0013 Log<br>Abundance")
+scatterPlot(icrData, xCName, yCName, colorCName="EM0013_sample", logColorCol = FALSE, legendTitle="EM0013<br>Abundance")
+
+# things that should fail
+scatterPlot(icrData, "not a real column", yCName, colorPal="#FF0000")
+scatterPlot(icrData, xCName, "yCName", colorPal="#FF0000")
+scatterPlot(icrData, xCName, "Class", colorPal="#FF0000")
+scatterPlot(icrData, "Class", yCName, colorPal="#FF0000")
+
+
