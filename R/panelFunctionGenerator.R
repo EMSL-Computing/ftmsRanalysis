d06ade7cbbdb7ad83f6e9c7da758cfb0886977a8
Amanda White <Amanda.White@pnnl.gov>
2018-06-27 17:41:33 -0700

    Added example to panelFunctionGenerator documentation

--- R/panelFunctionGenerator.R
+++ R/panelFunctionGenerator.R
@@ -2,7 +2,35 @@
 #'
 #' @param plot_fn_name plot function name, e.g. "vanKrevelenPlot" or "kendrickPlot"
 #' @param ... other parameters to pass to the plotting function other than the data object
+#' 
+#' @details Sometimes the additional parameters to pass to the plotting function will depend
+#' on the key associated with the data in the ddo (e.g. if a column name is prepended with 
+#' the group name). In that case, see example below for how
+#' to construct an \code{expr} statement to obtain the necessary information.
+#' 
+#' @author Amanda White
+#' 
 #' @export
+#' 
+#' @examples
+#' library(fticRanalysis)
+#' library(trelliscope)
+#' 
+#' vdbDir <- vdbConn(file.path(tempdir(), "trell_test"), autoYes = TRUE) #temporary directory
+#' 
+#' data('peakIcrProcessed')
+#' groupDdo <- divideByGroup(peakIcrProcessed)
+#' groupSummaryDdo <- summarizeGroups(groupDdo, summary_functions = c("prop_present", "n_present"))
+#' 
+#' # See expr here:
+#' panelFnG1 <- panelFunctionGenerator("vanKrevelenPlot", colorCName=expr(paste0(getSplitVar(v, "Group"), "_prop_present")), 
+#'                                     legendTitle="Proportion<br>Present")
+#' 
+#' makeDisplay(groupSummaryDdo, 
+#'             panelFn=panelFnG1,
+#'             name = "Trelliscope test G_1 with VK plot per group",
+#'             group = "Group")
+#' view()
 panelFunctionGenerator <- function(plot_fn_name, ...) {
   if (missing(plot_fn_name)) stop("plot_fn_name must be provided")
   if (!is.character(plot_fn_name)) stop("plot_fn_name must be character")
