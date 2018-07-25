6873b5c9fc1a2c8cdee07586127b9c960cd557ab
Amanda White <amanda.white@pnnl.gov>
2018-07-24 17:57:26 -0700

    initial commit of summary.peakIcrData function

---
+++ R/summary.R
@@ -0,0 +1,28 @@
+#' Summary of icrData object
+#'
+#' @param icrData object of type icrData
+#'
+#' @return list object containing summary statistics
+#' @export
+summary.icrData <- function(icrData) {
+  
+  res <- list()
+  samp_names <- unique(icrData$f_data[, getFDataColName(icrData)])
+  res$Samples <- length(samp_names)
+  res$Molecules <- nrow(icrData$e_data)
+  data_vals <- icrData$e_data[, samp_names]
+  if (getDataScale(icrData) == "abundance" | getDataScale(icrData) == "pres") {
+    nmissing <- sum(data_vals == 0 | is.na(data_vals))
+  } else {
+    nmissing <- sum(is.na(data_vals))
+  }
+  res$Percent_Missing <- nmissing/prod(dim(data_vals))*100
+  
+  class(res) <- "icrDataSummary"
+  return(res)
+}
+
+#' @export
+print.icrDataSummary <- function(obj) {
+  cat(sprintf("Samples: %d\nMolecules: %d\nPercent Missing: %.3f%%\n", obj$Samples, obj$Molecules, obj$Percent_Missing))
+}
