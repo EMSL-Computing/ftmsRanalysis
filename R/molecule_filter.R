7f6b06726ddac855623b261e42218f151becc784
Amanda White <amanda.white@pnnl.gov>
2018-08-10 15:59:15 -0700

    fixed bug

--- R/molecule_filter.R
+++ R/molecule_filter.R
@@ -23,9 +23,10 @@ molecule_filter <- function(icrData){
   if(!inherits(icrData,"peakIcrData") & !inherits(icrData, "compoundIcrData")) stop("icrData must be of class 'peakIcrData' or 'compoundIcrData'")
   
   edata_id = getEDataColName(icrData)
+  sample_cols <- as.character(icrData$f_data[, getFDataColName(icrData)])
 
   # get row sums of nonmiss = number of times each feature is observed #
-  num_obs <- n_present(icrData$e_data[,-edata_id], data_scale = getDataScale(icrData))
+  num_obs <- n_present(icrData$e_data[,sample_cols], data_scale = getDataScale(icrData))
 
   # output #
   output <- data.frame(icrData$e_data[, edata_id], num_obs)
