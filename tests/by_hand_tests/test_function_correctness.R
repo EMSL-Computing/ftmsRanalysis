14be284ba883e579f9cf79faf4c9c44b60d2394f
Amanda White <amanda.white@pnnl.gov>
2018-08-10 16:00:44 -0700

    attempt to identify global variable use

---
+++ tests/by_hand_tests/test_function_correctness.R
@@ -0,0 +1,38 @@
+## This script attemps to identify functions using global variables in
+## the fticRanalysis namespace using the codetools library. However,
+## this cannot correctly identify variable names used in non-standard
+## evaluation (e.g. column name usage in dplyr), so the printed 
+## messages are only an indication of potential problems.
+## 
+## Amanda M. White (8/10/2018)
+
+library(codetools)
+
+# load all libraries used in fticRanalysis
+imports <- packageDescription("fticRanalysis")$Imports
+imports <- gsub(" \\(.*\\)", "", unlist(strsplit(imports, ",?\n")))
+
+suggests <- packageDescription("fticRanalysis")$Suggests
+suggests <- gsub(" \\(.*\\)", "", unlist(strsplit(suggests, ",?\n")))
+
+for (ll in c(imports, suggests)) do.call(library, list(ll))
+
+library(fticRanalysis)
+
+ns_exports <- getNamespaceExports(asNamespace("fticRanalysis"))
+for (ns_name in ns_exports) {
+  obj <- getFromNamespace(ns_name, ns="fticRanalysis")
+  if (mode(obj) != "function") next
+  
+  res <- codetools::findGlobals(obj, merge = FALSE)
+  
+  if (length(res$variables) > 0 ) {
+  # expect(length(res$variables) == 0, 
+    ok <-unlist(lapply(res$variables, exists, mode="function"))
+    if (any(!ok)) {
+      #browser()
+      cat(
+           sprintf("Found possible global variable(s) in function %s: [%s]", ns_name, paste(res$variables[!ok], collapse=", ")), "\n")
+    }
+  }
+}
