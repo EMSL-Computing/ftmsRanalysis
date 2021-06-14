8e0deb610a508166257adfa187081dc7ca373363
Natalie-Winans <natalie.winans@gmail.com>
2021-06-14 15:25:31 -0700

    Add default formula colname

--- R/read_CoreMS_data.R
+++ R/read_CoreMS_data.R
@@ -16,17 +16,17 @@
 #' @export
 
 read_CoreMS_data <- function(list_of_files, force_rbind = FALSE) {
-
+  
   # Check that list_of_files is a vector of strings
   if(!is.character(list_of_files)) stop("list_of_files must be of the class 'chr'")
-
+  
   read_add_filename <- function(file_name) {
     readr::read_csv(file_name) %>%
       dplyr::mutate(Filename = file_name)
   }
-
+  
   file_list <- lapply(list_of_files, read_add_filename)
-
+  
   # Check that columns are consistent across files
   df_cnames <- list()
   for (i in 1:length(file_list)) {
@@ -34,21 +34,21 @@ read_CoreMS_data <- function(list_of_files, force_rbind = FALSE) {
   }
   col_nums <- table(unlist(df_cnames))
   exclude_cols <- names(which(col_nums < length(file_list)))
-
+  
   if (force_rbind) {
     all_data <- dplyr::bind_rows(file_list) %>%  dplyr::select(-exclude_cols)
   } else if (length(exclude_cols) > 0) {
-      stop("one or more columns not present in all files")
+    stop("one or more columns not present in all files")
   } else {
     all_data <- dplyr::bind_rows(file_list)
   }
-
+  
   if ("X1" %in% names(all_data)) {
     all_data <- all_data %>% dplyr::select(-X1)
   }
-
+  
   class(all_data) <- append(class(all_data), "CoreMSrbind")
-
+  
   return(all_data)
 }
 
@@ -90,28 +90,30 @@ as.CoreMSData <- function(all_data,
                           error_cname = "Mass Error (ppm)",
                           conf_cname = "Confidence Score",
                           file_cname = "Filename",
-                          mf_cname = NULL, c13_cname = NULL,
-                          s34_cname = NULL, o18_cname = NULL,
+                          mf_cname = "Molecular Formula",
+                          c13_cname = NULL,
+                          s34_cname = NULL,
+                          o18_cname = NULL,
                           n15_cname = NULL){
-
+  
   # check that all_data is of class 'CoreMSrbind'
   if(!inherits(all_data, "CoreMSrbind")) stop("all_data must be of the class 'CoreMSrbind'")
-
+  
   # check that required columns exist in all_data
   if(!(mass_cname %in% names(all_data))) stop(paste("Calculated mass column", mass_cname,"not found in all_data"))
   if(!(pheight_cname %in% names(all_data))) stop(paste("Peak height/intensity column", pheight_cname,"not found in all_data"))
   if(!(error_cname %in% names(all_data))) stop(paste("Mass error column", error_cname,"not found in all_data"))
   if(!(conf_cname %in% names(all_data))) stop(paste("Confidence score column", conf_cname,"not found in all_data"))
   if(!(file_cname %in% names(all_data))) stop(paste("Filename column", file_cname,"not found in all_data"))
-
+  
   # check that cname arguments are found in all_data
-if(!is.null(mf_cname)){
+  if(!is.null(mf_cname)){
     if(!(mf_cname %in% names(all_data))) stop(paste("Molecular Formula column", mf_cname, "not found in all_data"))
   }
   if(!is.null(c13_cname)){
     if(!(c13_cname %in% names(all_data))) stop(paste("Carbon column", c13_cname, "not found in all_data"))
   }
-if(!is.null(o18_cname)){
+  if(!is.null(o18_cname)){
     if(!(o18_cname %in% names(all_data))) stop(paste("Oxygen column", o18_cname, "not found in all_data"))
   }
   if(!is.null(n15_cname)){
@@ -120,15 +122,15 @@ if(!is.null(o18_cname)){
   if(!is.null(s34_cname)){
     if(!(s34_cname %in% names(all_data))) stop(paste("Sulfur column", s34_cname, "not found in all_data"))
   }
-
+  
   attr(all_data, "cnames") <- list(mass_cname = mass_cname, pheight_cname = pheight_cname,
                                    error_cname = error_cname, conf_cname = conf_cname,
                                    file_cname = file_cname, mf_cname = mf_cname,
                                    c13_cname = c13_cname, o18_cname = o18_cname,
                                    s34_cname = s34_cname, n15_cname = n15_cname)
-
-
+  
+  
   class(all_data) <- c("CoreMSData", "data.frame")
-
+  
   return(all_data)
 }
