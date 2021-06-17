d60b6cc40580ca13db4ee2d230e1685583003b2b
Natalie-Winans <natalie.winans@gmail.com>
2021-06-17 16:57:10 -0700

    Added conf_filter function

---
+++ R/conf_filter.R
@@ -0,0 +1,27 @@
+#' Create conf\_filter object
+#'
+#' @param cmsObj an object of class 'CoreMSData', should be ouput of the function 
+#' \code{as.CoreMSData}
+#'
+#' @return object of class 'confFilt' 
+#' 
+#' 
+#' @export
+
+conf_filter <- function(cmsObj) {
+  
+  if(!inherits(cmsObj, "CoreMSData")) stop("cmsObj must be of class 'CoreMSData")
+  
+  conf_id <- attr(cmsObj, "cnames")$conf_cname
+  mass_id  <- attr(cmsObj, "cnames")$mass_cname
+  error_id <- attr(cmsObj, "cnames")$error_cname
+  file_id <- attr(cmsObj, "cnames")$file_cname
+  formula_id <- attr(cmsObj, "cnames")$mf_cname
+  
+  output <- dat %>% select(mass_id, conf_id, error_id, formula_id, file_id)
+  
+  class(output) <- c("confFilt", "data.frame")
+  
+  return(output)
+  
+}
