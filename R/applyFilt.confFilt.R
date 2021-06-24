d21f3bd0a8c6e5a68151d463a81b18689ac22204
Natalie-Winans <natalie.winans@gmail.com>
2021-06-24 09:05:07 -0700

    Add comments/documentation

--- R/applyFilt.confFilt.R
+++ R/applyFilt.confFilt.R
@@ -1,16 +1,20 @@
-#' applyFilt function for confFilt
+#' Apply an S3 filter object to a CoreMSData S3 object
+#' 
+#' This function takes a filter object of class 'confFilt' and applies the filter to a dataset of class \code{CoreMSData}
 #'
-#' @param filter_object 
-#' @param cmsObj an object of the class 'CoreMSData', the output of \code{as.CoreMSData}
+#' @param filter_object an object of class 'confFilt' created by \code{\link{conf_filter}}
+#' @param cmsObj an object of class 'CoreMSData', the output of \code{\link{as.CoreMSData}}
 #' @param min_conf a numeric value greater than 0 specifying the minimum confidence score a peak should have in order to be retained
 #'
 #' @return an object of the class 'CoreMSData' that has been filtered by confidence score
+#' 
+#' @author Natalie Winans
 #' 
 #' @export
 applyFilt.confFilt <- function(filter_object, cmsObj, min_conf = 0.5) {
 
   if ("confFilt" %in% names(attr(cmsObj, "filters"))) {
-    prev_min_conf <- attr(cmsObj, "filters")$confFilt$threshold
+    prev_min_conf <- attr(cmsObj, "filters")$confFilt$minimum
 
     stop(paste0("A confidence filter has already been run on this dataset using a 'min_conf' of ", prev_min_conf))
 
@@ -25,7 +29,10 @@ applyFilt.confFilt <- function(filter_object, cmsObj, min_conf = 0.5) {
     mass_cname <- attr(cmsObj, "cnames")$mass_cname
     conf_cname <- attr(cmsObj, "cnames")$conf_cname
 
+    # get peaks to keep
     filtered_cmsObj <- cmsObj %>% dplyr::filter(cmsObj[,conf_cname] >= min_conf)
+    
+    # get mass/ID of peaks to remove
     peaks_removed <- cmsObj %>% 
       dplyr::filter(cmsObj[,conf_cname] < min_conf | is.na(cmsObj[,conf_cname])) %>% 
       dplyr::select(mass_cname)
@@ -36,6 +43,7 @@ applyFilt.confFilt <- function(filter_object, cmsObj, min_conf = 0.5) {
     
     cmsObj <- filtered_cmsObj
     
+    # add 'filters' attributes
     attr(cmsObj, "filters")$confFilt <- list(report_text = "", minimum = c(), removed = c())
     attr(cmsObj, "filters")$confFilt$report_text <- paste0("A confidence filter was applied to the data, removing peaks with a confidence score of less than ", min_conf, ". A total of ", num_rmv, " rows were removed by this filter.")
     attr(cmsObj, "filters")$confFilt$minimum <- min_conf
