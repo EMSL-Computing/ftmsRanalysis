6300e86b9d67d52b9135a29ce7f2254eb7867390
PNL\wina135 <natalie.winans@pnnl.gov>
2021-07-26 13:37:17 -0500

    Added NA count and changed "minimum" to "threshold"
    Added conf_cname attribute to confFilt object
    
    Added parameter checks and made plot interactive

--- R/plot.CoreMSData.R
+++ R/plot.CoreMSData.R
@@ -6,9 +6,9 @@
 #' @param title optional, if not specified "Unique Masses per Sample" will be used
 #' @param xlabel optional, if not specified "Sample" will be used
 #' @param ylabel optional, if not specified "Unique Masses" will be used
-#' @param long_sample_names logical, optionally angles sample names on x-axis if names are long
+#' @param diag_x_labs logical, optionally angles sample names on x-axis for readability if there are many samples and/or long sample names
 #'
-#' @return a `ggplot` object
+#' @return `plotly` object
 #' @export
 #'
 #' @examples
@@ -16,9 +16,22 @@ plot.CoreMSData <- function(cmsObj,
                             title = "Unique Masses per Sample", 
                             xlabel = "Sample", 
                             ylabel = "Unique Masses",
-                            long_sample_names = FALSE) {
+                            diag_x_labs = FALSE) {
   
+  # Check that cmsObj is a "CoreMSData" object (output from function as.CoreMSData())
   if(!inherits(cmsObj, "CoreMSData")) stop("cmsObj must be of class 'CoreMSData'")
+  
+  # Check that that parameter inputs are valid
+  if(!is.null(title)) {
+    if(!inherits(title, "character") | !(length(title) == 1)) stop("title must be single character string")
+  }
+  if(!is.null(xlabel)) {
+    if(!inherits(xlabel, "character") | !(length(xlabel) == 1)) stop("xlabel must be single character string")
+  }
+  if(!is.null(ylabel)) {
+    if(!inherits(ylabel, "character") | !(length(ylabel) == 1)) stop("ylabel must be single character string")
+  }
+  if(!(diag_x_labs == TRUE | diag_x_labs == FALSE)) stop("diag_x_labs must be logical argument")
   
   sample_id <- attr(cmsObj, "cnames")$file_cname
   mass_id <- attr(cmsObj, "cnames")$mass_cname 
@@ -27,11 +40,9 @@ plot.CoreMSData <- function(cmsObj,
     dplyr::group_by(dplyr::pull(cmsObj, sample_id)) %>%
     dplyr::distinct(dplyr::pull(cmsObj, mass_id)) %>%
     dplyr::tally()
-  
-  print(unique_masses_per_sample)
 
   plot <- unique_masses_per_sample %>%
-    ggplot2::ggplot(aes(x = `dplyr::pull(cmsObj, sample_id)`,
+    ggplot2::ggplot(ggplot2::aes(x = `dplyr::pull(cmsObj, sample_id)`,
                         y = n,
                         label = n)) +
     ggplot2::geom_bar(stat = "identity") +
@@ -41,10 +52,10 @@ plot.CoreMSData <- function(cmsObj,
                   y = ylabel) +
     ggplot2::geom_text(nudge_y = 1.5)
   
-  if (long_sample_names == TRUE) {
+  if (diag_x_labs == TRUE) {
     plot <- plot +
       ggplot2::theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
   }
 
-  return(plot)
+  plotly::ggplotly(plot)
 }
