2dd94cdb640f75c95a892e2d326836733e8e969f
Natalie-Winans <natalie.winans@gmail.com>
2022-01-13 09:16:55 -0600

    Added function to create filtering data table for Shiny app

---
+++ R/conf_filter_dt.R
@@ -0,0 +1,34 @@
+#' Confidence Filter Data Table
+#' 
+#' Creates a data table for confidence filtering in Shiny app
+#'
+#' @param min_conf numeric value between 0 and 1 denoting minimum confidence threshold
+#' @param cmsObj CoreMSData object created using \link{`as.CoreMSData`}
+#'
+#' @return `data.frame` containg counts of remaining and removed monoisotopic peaks in each file/sample, based on selected minimum confidence threshold
+#'
+#' @examples
+conf_filter_dt <- function(cmsObj, min_conf) {
+  
+  conf_score <- attr(cmsObj, "cnames")$conf_cname
+  filename <- attr(cmsObj, "cnames")$file_cname
+  
+  orig_peaks <- cmsObj$monoiso_data %>% 
+    dplyr::group_by(.data[[filename]]) %>% 
+    dplyr::count() %>% 
+    dplyr::rename(orig_n_peaks = n, Sample = .data[[filename]])
+
+  dt <- cmsObj$monoiso_data %>% 
+    dplyr::filter(.data[[conf_score]] >= min_conf) %>% 
+    dplyr::group_by(.data[[filename]]) %>% 
+    dplyr::count() %>%
+    dplyr::rename(`Peaks Remaining` = n, Sample = .data[[filename]]) 
+
+  cf_dt <- suppressMessages(dplyr::left_join(orig_peaks, dt)) %>%
+    dplyr::mutate(`Peaks Remaining (%)` = round((`Peaks Remaining`/orig_n_peaks)*100, 1),
+                  `Peaks Removed` = orig_n_peaks - `Peaks Remaining`,
+                  `Peaks Removed (%)` = round((orig_n_peaks - `Peaks Remaining`)/orig_n_peaks*100, 1)) %>%
+    dplyr::select(!orig_n_peaks)
+
+  return(cf_dt)
+}
