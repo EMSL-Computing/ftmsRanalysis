0b5dd152ea2798b925b307a596a4afdb9767e699
Natalie-Winans <natalie.winans@gmail.com>
2022-01-13 13:08:57 -0600

    Update exampleCoreMSData with new colnames attributes
    Changed to use exampleCoreMSData, added error out tests
    
    Added tests for CoreMSData_to_ftmsData
    
    Updates to vignette

--- tests/testthat/test_conf_filter.R
+++ tests/testthat/test_conf_filter.R
@@ -1,19 +1,19 @@
 library(ftmsRanalysis)
 
+data("exampleCoreMSData")
+
+test_that("breaks when given incorrect input", {
+  expect_error(conf_filter(exampleCoreMSData$monoiso_data), "cmsObj must be of class 'CoreMSData")
+})
+
 test_that("confidence filter works correctly on CoreMSData", {
-  files <- list.files(pattern = "example")
-  cmsDat <- read_CoreMS_data(files)
-  cmsDat <- as.CoreMSData(cmsDat, 
-                          c13_cname = "13C",
-                          o18_cname = "18O")
-  
-  filtData <- conf_filter(cmsDat)
+  filtData <- conf_filter(exampleCoreMSData)
   expect_true(inherits(filtData, "confFilt"))
   expect_true(inherits(filtData, "list"))
   expect_true(ncol(filtData$iso_data) == 5)
   expect_true(ncol(filtData$monoiso_data) == 5)
   
-  cmsObj_filt <- applyFilt(filtData, cmsDat, min_conf = 0.5)
+  cmsObj_filt <- applyFilt(filtData, exampleCoreMSData, min_conf = 0.5)
   expect_true(inherits(cmsObj_filt, "CoreMSData"))
   expect_true(inherits(cmsObj_filt, "list"))
   new_confs <- rbind(cmsObj_filt$monoiso_data[,attr(cmsObj_filt, "cnames")$conf_cname], 
@@ -22,6 +22,5 @@ test_that("confidence filter works correctly on CoreMSData", {
   
   expect_true(!is.null(attr(cmsObj_filt, "filters")))
   expect_true(!is.null(attr(cmsObj_filt, "filters")$confFilt))
-  
   
 })
