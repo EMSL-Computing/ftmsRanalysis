de2d7d3732c8f86c5a679c73d901a6ebbfe51fdc
Natalie-Winans <natalie.winans@gmail.com>
2022-01-13 09:57:49 -0600

    Changed to take exampleCoreMSData, work with newer CoreMSData list format

--- tests/testthat/test_conf_filter.R
+++ tests/testthat/test_conf_filter.R
@@ -1,17 +1,23 @@
 library(ftmsRanalysis)
 
 test_that("confidence filter works correctly on CoreMSData", {
-  data("exampleCoreMSData")
+  files <- list.files(pattern = "example")
+  cmsDat <- read_CoreMS_data(files)
+  cmsDat <- as.CoreMSData(cmsDat, 
+                          c13_cname = "13C",
+                          o18_cname = "18O")
   
-  filtData <- conf_filter(exampleCoreMSData)
+  filtData <- conf_filter(cmsDat)
   expect_true(inherits(filtData, "confFilt"))
-  expect_true(inherits(filtData, "data.frame"))
-  expect_true(ncol(filtData) == 5)
+  expect_true(inherits(filtData, "list"))
+  expect_true(ncol(filtData$iso_data) == 5)
+  expect_true(ncol(filtData$monoiso_data) == 5)
   
-  cmsObj_filt <- applyConfFilt(filtData, exampleCoreMSData, min_conf = 0.5)
+  cmsObj_filt <- applyFilt(filtData, cmsDat, min_conf = 0.5)
   expect_true(inherits(cmsObj_filt, "CoreMSData"))
-  expect_true(inherits(cmsObj_filt, "data.frame"))
-  new_confs <- cmsObj_filt[,attr(cmsObj_filt, "cnames")$conf_cname]
+  expect_true(inherits(cmsObj_filt, "list"))
+  new_confs <- rbind(cmsObj_filt$monoiso_data[,attr(cmsObj_filt, "cnames")$conf_cname], 
+                 cmsObj_filt$iso_data[,attr(cmsObj_filt, "cnames")$conf_cname])
   expect_true(all(new_confs >= 0.5))
   
   expect_true(!is.null(attr(cmsObj_filt, "filters")))
